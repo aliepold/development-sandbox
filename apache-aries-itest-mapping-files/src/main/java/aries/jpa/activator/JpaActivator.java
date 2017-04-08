70eb7d7a6ec84db2fdb6a38465667c30211f3940
aliepold <al3x_java@gmx.de>
2017-04-09 00:13:00 +0200

    Sample Project with Apache Aries. Integration-Test für https://issues.apache.org/jira/browse/ARIES-1711

---
+++ apache-aries-itest-mapping-files/src/main/java/aries/jpa/activator/JpaActivator.java
@@ -0,0 +1,34 @@
+package aries.jpa.activator;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import aries.jpa.mapping.MappingService;
+import aries.jpa.mapping.MappingServiceImpl;
+
+public class JpaActivator implements BundleActivator {
+
+	private ServiceRegistration<?> serviceRegistration;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+
+		Dictionary<String, Object> props = new Hashtable<String, Object>();
+
+		props.put("osgi.command.scope", "jpa");
+		props.put("osgi.command.function", new String[] { "listCustomer", "listDeveloper", "listTester" });
+
+		this.serviceRegistration = context.registerService(MappingService.class.getName(), new MappingServiceImpl(), props);
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		this.serviceRegistration.unregister();
+	}
+
+}
